49b0d1b8c260fe1b3ce46b778d51a6a191c41dd0
Elvan Hidayat <elvanhd@gmail.com>
2024-03-26 00:36:09 +0700

    Integrate Vuex actions and getters with components
    - Connect `ControlCenter.vue` to Vuex store to dispatch `increment` action
    - Utilize `useStore` hook from Vuex in `ControlCenter.vue` for store access
    - Implement increment functionality in `ControlCenter.vue` by dispatching the `increment` Vuex action upon button click
    - Bind `TheCounter.vue` component to Vuex store for dynamic counter updates
    - Use `computed` property in `TheCounter.vue` to reactively fetch the counter value from Vuex getters
    - Ensure real-time counter state reflection in `TheCounter.vue` through Vuex state management

--- src/components/ControlCenter.vue
+++ src/components/ControlCenter.vue
@@ -3,9 +3,15 @@
 </template>
 
 <script>
+import { useStore } from 'vuex';
+
 export default {
   setup() {
-    function inc() {}
+    const store = useStore();
+
+    function inc() {
+      store.dispatch('increment');
+    }
 
     return { inc };
   },
